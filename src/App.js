22f241fe54abc4bf50c358b9a07906fc78c9d460
simonGreenwood <simonrobertgreenwood@gmail.com>
2022-04-01 14:16:24 +0000

    created new app

--- src/App.js
+++ src/App.js
@@ -10,14 +10,13 @@ export default function App() {
   
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
         👋 Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I'm Simon and I like full stack and web3 development!
         </div>
 
         <button className="waveButton" onClick={wave}>
